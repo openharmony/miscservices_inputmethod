ed2606272a23a7252ca773ce614b67137df411fc
zhouyongfei <zhouyongfei@huawei.com>
2021-10-20 16:25:49 +0800

    jnap改到ui线程处理，修复输入法概率白屏的问题
    Signed-off-by: zhouyongfei <zhouyongfei@huawei.com>

--- frameworks/inputmethod_ability/src/event_target.cpp
+++ frameworks/inputmethod_ability/src/event_target.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include <uv.h>
 #include "event_target.h"
 
 #include "securec.h"
@@ -28,6 +28,15 @@ namespace MiscServices {
         napi_ref handlerRef = nullptr;
         EventListener *back = nullptr;
         EventListener *next = nullptr;
+    };
+    struct EventTargetCB {
+        napi_env env;
+        napi_ref thisVarRef;
+        EventListener *first;
+        EventListener *last;
+        sptr<EventTarget> &eventTarget;
+        const char *type;
+        Event *event;
     };
     EventTarget::EventTarget(napi_env env, napi_value thisVar)
     {
@@ -159,27 +168,63 @@ namespace MiscServices {
         }
     }
 
-    void EventTarget::Emit(const char *type, Event *event)
+    void EventTarget::Emit(sptr<EventTarget> &eventTarget, const char *type, Event *event)
     {
         IMSA_HILOGI("EventTarget::Emit");
-        napi_handle_scope scope = nullptr;
-        napi_open_handle_scope(env_, &scope);
-
-        napi_value thisVar = nullptr;
-        napi_get_reference_value(env_, thisVarRef_, &thisVar);
-        for (EventListener *eventListener = first_; eventListener != nullptr; eventListener = eventListener->next) {
-            if (strcmp(eventListener->type, type) == 0) {
-                napi_value jsEvent = event ? event->ToJsObject() : nullptr;
-                napi_value handler = nullptr;
-                napi_value result = nullptr;
-                napi_get_reference_value(env_, eventListener->handlerRef, &handler);
-                napi_call_function(env_, thisVar, handler, jsEvent ? 1 : 0, jsEvent ? &jsEvent : nullptr, &result);
-                if (eventListener->isOnce) {
-                    Off(type, handler);
-                }
-            }
+        uv_loop_s *loop = nullptr;
+        napi_get_uv_event_loop(env_, &loop);
+        if (loop == nullptr) {
+            IMSA_HILOGI("EventTarget::Emit loop == nullptr");
+            return;
         }
-        napi_close_handle_scope(env_, scope);
+
+        uv_work_t *work = new (std::nothrow) uv_work_t;
+        if (work == nullptr) {
+            IMSA_HILOGI("EventTarget::Emit No memory work == nullptr");
+            return;
+        }
+
+        EventTargetCB *eventTargetCB = new (std::nothrow) EventTargetCB {.env = env_, .thisVarRef = thisVarRef_,
+            .first = first_, .last = last_, .type = type, .event = event, .eventTarget = eventTarget};
+
+        work->data = (void *)eventTargetCB;
+
+        int ret = uv_queue_work(loop, work, [](uv_work_t *work) {}, [](uv_work_t *work, int status) {
+            //Js Thread
+            if (work == nullptr) {
+                IMSA_HILOGI("EventTarget::Emit work == nullptr");
+                return;
+            }
+            EventTargetCB *eventTargetCB = (EventTargetCB *)work->data;
+            do {
+                napi_handle_scope scope = nullptr;
+                napi_open_handle_scope(eventTargetCB->env, &scope);
+
+                napi_value thisVar = nullptr;
+                napi_get_reference_value(eventTargetCB->env, eventTargetCB->thisVarRef, &thisVar);
+                for (EventListener *eventListener = eventTargetCB->first; eventListener != nullptr; eventListener = eventListener->next) {
+                    if (strcmp(eventListener->type, eventTargetCB->type) == 0) {
+                        napi_value jsEvent = eventTargetCB->event ? eventTargetCB->event->ToJsObject() : nullptr;
+                        napi_value handler = nullptr;
+                        napi_value result = nullptr;
+                        napi_get_reference_value(eventTargetCB->env, eventListener->handlerRef, &handler);
+                        napi_call_function(eventTargetCB->env, thisVar, handler, jsEvent ? 1 : 0, jsEvent ? &jsEvent : nullptr, &result);
+                        if (eventListener->isOnce) {
+                            eventTargetCB->eventTarget->Off(eventTargetCB->type, handler);
+                        }
+                    }
+                }
+                napi_close_handle_scope(eventTargetCB->env, scope);
+            } while (0);
+            if (eventTargetCB) {
+                delete eventTargetCB;
+            }
+            delete work;
+        });
+        if (ret != 0) {
+            IMSA_HILOGI("EventTarget::Emit failed to execute libuv work queue");
+            delete work;
+        }
     }
 }
 }
