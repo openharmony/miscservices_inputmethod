eafade8741e747b06cbef918611f192620fddf4c
zhouyongfei <zhouyongfei@huawei.com>
2021-12-03 12:00:51 +0800

    修复静态检查问题
    Signed-off-by: zhouyongfei <zhouyongfei@huawei.com>

--- frameworks/inputmethod_controller/include/input_method_utils.h
+++ frameworks/inputmethod_controller/include/input_method_utils.h
@@ -1,14 +1,24 @@
-//
-// Created by OpenHarmonyUser on 2021/11/30.
-//
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef FM_IMC_PROJECT_INPUT_METHOD_UTILS_H
 #define FM_IMC_PROJECT_INPUT_METHOD_UTILS_H
 
-#endif //FM_IMC_PROJECT_INPUT_METHOD_UTILS_H
 namespace OHOS {
     namespace MiscServices {
-        class Configuration{
+        class Configuration {
         public:
             enum {
                 TEXT_FIELD_ACTION_UNSPECIFIED = 0,
@@ -22,11 +32,13 @@ namespace OHOS {
             };
             int mTextFieldAction;
         };
-        class CursorInfo{};
-        class KeyEvent{};
-        class KeyboardStatus{
+        class CursorInfo {
+        };
+        class KeyEvent {
+        };
+        class KeyboardStatus {
         public:
-            enum{
+            enum {
                 KEYBOARD_STATUS_HIDE = 0,
                 KEYBOARD_STATUS_SHOW
             };
@@ -34,3 +46,4 @@ namespace OHOS {
         };
     }
 }
+#endif // FM_IMC_PROJECT_INPUT_METHOD_UTILS_H
