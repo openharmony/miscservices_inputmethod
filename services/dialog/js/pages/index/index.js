87f6b0ab20674549129ebbc5a986d4e0b7ebed0f
zhouyongfei <zhouyongfei@huawei.com>
2022-02-11 19:03:17 +0800

    合入输入法选择对话框布局
    Signed-off-by: zhouyongfei <zhouyongfei@huawei.com>

---
+++ services/dialog/js/pages/index/index.js
@@ -0,0 +1,19 @@
+import router from '@ohos.router'
+
+export default {
+    data: {
+        imeList: router.getParams().imeList,
+    },
+    onInit() {
+        this.dialogTitle = this.$t('strings.dialogTitle');
+        this.setIme = this.$t('strings.setIme');
+    },
+    changeDefaultIme(ime) {
+        console.info('ImsaKit-dialog changeDefaultIme: ' + ime);
+        callNativeHandler("EVENT_CHANGE_IME", ime);
+    },
+    startImeSetting() {
+        console.info('ImsaKit-dialog startImeSetting');
+        callNativeHandler("EVENT_START_IME_SETTING", "");
+    }
+}
