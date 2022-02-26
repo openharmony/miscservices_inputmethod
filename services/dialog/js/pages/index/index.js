a75243ce436ab265b08de3c5af25a099ac38774b
zhouyongfei <zhouyongfei@huawei.com>
2022-02-26 16:26:44 +0800

    更新输入法选择弹框、修改bug
    Signed-off-by: zhouyongfei <zhouyongfei@huawei.com>

--- services/dialog/js/pages/index/index.js
+++ services/dialog/js/pages/index/index.js
@@ -1,5 +1,4 @@
 import router from '@ohos.router'
-import resourceManager from '@ohos.resourceManager';
 
 export default {
     data: {
@@ -8,7 +7,6 @@ export default {
     onInit() {
         this.dialogTitle = this.$t('strings.dialogTitle');
         this.setIme = this.$t('strings.setIme');
-        this.initString()
     },
     changeDefaultIme(ime) {
         console.info('ImsaKit-dialog changeDefaultIme: ' + ime);
@@ -18,40 +16,4 @@ export default {
         console.info('ImsaKit-dialog startImeSetting');
         callNativeHandler("EVENT_START_IME_SETTING", "");
     },
-    initString() {
-        for (var i = 0; i < this.imeList.length; i++) {
-            let bundle = this.imeList[i].ime;
-            console.info('ImsaKit-dialog initString bundle ' + bundle);
-            let index = bundle.indexOf("/");
-            let pn = bundle.substring(0, index);
-            let labelId = Number(this.imeList[i].labelId);
-            let discriptionId = Number(this.imeList[i].discriptionId);
-            resourceManager.getResourceManager(pn).then(mgr => {
-                mgr.getString(labelId).then(value => {
-                    this.updateLabelData(bundle, value, '');
-                }).catch(error => {
-                    console.info("ImsaKit-dialog initString resource getString error:" + error);
-                })
-                mgr.getString(discriptionId).then(value => {
-                    this.updateLabelData(bundle, '', value);
-                }).catch(error => {
-                    console.info("ImsaKit-dialog initString resource getString error:" + error);
-                })
-            }).catch(error => {
-                console.info("ImsaKit-dialog initString getResourceManager error:" + error);
-            });
-        }
-    },
-    updateLabelData(bundle, label, discription) {
-        for (var i = 0; i < this.imeList.length; i++) {
-            if (this.imeList[i].ime == bundle) {
-                if (label != '') {
-                    this.imeList[i].label = label;
-                }
-                if (discription != '') {
-                    this.imeList[i].discription = discription;
-                }
-            }
-        }
-    }
 }
